477d0772b7106b7bbc513cb524144a3d74121c98
CharlieJiang <cqjjjzr@126.com>
2016-06-19 22:13:15 +0800

    Docs!

--- MinecraftMirrorClient/src/charlie/mirror/client/MirrorList.java
+++ MinecraftMirrorClient/src/charlie/mirror/client/MirrorList.java
@@ -8,6 +8,10 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * A class to store mirrors.
+ * @author Charlie Jiang
+ */
 public class MirrorList implements Iterable<MirrorInformation> {
     private List<MirrorInformation> mirrors = new LinkedList<>();
 
@@ -15,6 +19,10 @@ public class MirrorList implements Iterable<MirrorInformation> {
         mirrors.add(new MojangInformation());
     }
 
+    /**
+     * Add a mirror from a json.
+     * @param json The mirror json.
+     */
     public void add(String json){
         JSONObject rootObj = new JSONObject(json);
         JSONArray array = rootObj.getJSONArray("mirrors");
@@ -31,12 +39,29 @@ public class MirrorList implements Iterable<MirrorInformation> {
         mirrors.add(0, information);
     }
 
+    /**
+     * Remove a mirror from the list with a name.
+     * @param name The name of the mirror.
+     */
     public void remove(String name){
         for (MirrorInformation mirror : mirrors) {
             if(mirror.getName().equals(name)) mirrors.remove(mirror);
         }
     }
 
+    /**
+     * Get the size of the list.
+     * @return The size.
+     */
+    public int size(){
+        return mirrors.size();
+    }
+
+    /**
+     * Get a mirror from a name.
+     * @param name The name.
+     * @return The mirror.
+     */
     public MirrorInformation get(String name){
         for (MirrorInformation mirror : mirrors) {
             if(mirror.getName().equals(name)) return mirror;
