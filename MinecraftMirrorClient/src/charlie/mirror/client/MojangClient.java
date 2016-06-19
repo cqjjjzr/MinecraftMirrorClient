d4c99247ef41348c33526b9acf250c2eb4483f1c
CharlieJiang <cqjjjzr@126.com>
2016-06-19 21:47:52 +0800

    Error tagging.

--- MinecraftMirrorClient/src/charlie/mirror/client/MojangClient.java
+++ MinecraftMirrorClient/src/charlie/mirror/client/MojangClient.java
@@ -1,9 +1,13 @@
 package charlie.mirror.client;
 
 import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MojangClient {
     private MirrorList list;
+    private List<String> errorURL = new LinkedList<>();
+    private List<String> errorMirror = new LinkedList<>();
 
     public MojangClient(MirrorList list){
         this.list = list;
@@ -11,8 +15,10 @@ public class MojangClient {
 
     public String getVersion(String offcialURL){
         for(MirrorInformation information : list){
-            if(information.getVersionURL() != null){
-                return MojangMirror.getVersion(information, MojangMirror.parseVersion(URI.create(offcialURL))).toString();
+            if(information.getVersionURL() != null && errorMirror.contains(information.getName())){
+                String url = MojangMirror.getVersion(information, MojangMirror.parseVersion(URI.create(offcialURL))).toString();
+                if(errorURL.contains(url)) continue;
+                return url;
             }
         }
         return null;
@@ -20,8 +26,10 @@ public class MojangClient {
 
     public String getAssets(String hash){
         for(MirrorInformation information : list){
-            if(information.getAssetsURL() != null){
-                return MojangMirror.getAsset(information, hash).toString();
+            if(information.getAssetsURL() != null && errorMirror.contains(information.getName())){
+                String url = MojangMirror.getAsset(information, hash).toString();
+                if(errorURL.contains(url)) continue;
+                return url;
             }
         }
         return null;
@@ -29,10 +37,22 @@ public class MojangClient {
 
     public String getLibraries(String offcialURL){
         for(MirrorInformation information : list){
-            if(information.getLibrariesURL() != null){
-                return MojangMirror.getLibrary(information, MojangMirror.parseLibrary(URI.create(offcialURL))).toString();
+            if(information.getLibrariesURL() != null && errorMirror.contains(information.getName())){
+                String url = MojangMirror.getLibrary(information, MojangMirror.parseLibrary(URI.create(offcialURL))).toString();
+                if(errorURL.contains(url)) continue;
+                return url;
             }
         }
         return null;
+    }
+
+    public void addErrorURL(String url){
+        if(!errorURL.contains(url))
+            errorURL.add(url);
+    }
+
+    public void addErrorMirror(String mirror){
+        if(!errorMirror.contains(mirror))
+            errorMirror.add(mirror);
     }
 }
